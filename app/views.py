f92aae985d826176e63b7b941ed34a2b10f3878d
ndongchrist <christianhonore2003@gmail.com>
2024-03-07 22:26:03 +0100

    Added templates

--- app/views.py
+++ app/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Bonjour tout le monde")
+    return render(request, "app/index.html")
+
+def register(request):
+    return render(request, 'app/register.html')
+
+def login(request):
+    return render(request, 'app/login.html')
+
+def logout(request):
+    pass
+    # return render(request, 'app/logout.html')
