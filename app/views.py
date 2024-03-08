94fe19771c48ab7b5a4541b1a56e71327aef7112
ndongchrist <christianhonore2003@gmail.com>
2024-03-08 08:50:05 +0100

    Completed: Authentication with Confirmation Email Done

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from authentification.info import *
+from django.core.mail import send_mail, EmailMessage
+from .token import generatorToken
 
 # Create your views here.
 def home(request):
@@ -32,9 +39,38 @@ def register(request):
         my_user = User.objects.create_user(username, email, password)
         my_user.first_name = firstname
         my_user.last_name = lastname
+        my_user.is_active = False
         my_user.save()
-
         messages.success(request, 'Account Successfully Created')
+
+    #Welcome Email Sender
+    
+
+        subject = "Wleocome on GoldenBrain, Django System Login"
+        message =  "Welcome "+ my_user.first_name + " " + my_user.last_name + " \n We are proud tu count you amongst our users \n\n\n THank you \n\n GoldenDev"
+        from_email = EMAIL_HOST_USER
+        to_list = [my_user.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=False)
+
+    #Email Confirmation
+        current_site = get_current_site(request)
+        email_subject = "Confirm Your Email Address On GoldenBrain"
+        message_confirm = render_to_string('emailconfirm.html', {
+            'name': my_user.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(my_user.pk)),
+            'token': generatorToken.make_token(my_user)
+        })
+        email = EmailMessage(
+            email_subject,
+            message_confirm,
+            EMAIL_HOST_USER,
+            [my_user.email]
+        )
+
+        email.fail_silently = False
+        email.send()
+
         return redirect('login')
     else:
         pass
@@ -46,11 +82,15 @@ def logIn(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
+        my_user = User.objects.get(username = username)
+
         if user is not None:
             login(request, user)
             firstname = user.first_name
             messages.success(request, f"{firstname} Login was sucessful")
             return render(request, 'app/index.html', {'firstname': firstname})
+        elif my_user.is_active == False:
+            messages.error(request, "You haven't Confirmed your account")
         else:
             messages.error(request, "Login Unsucessful")
             return redirect('home')
@@ -62,3 +102,21 @@ def logOut(request):
     messages.success(request, "Logout sucessful")
     return redirect('home')
     # return render(request, 'app/logout.html')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+
+    except Exception as e:
+        user = None
+        messages.error(e)
+        return print("Activation Error")
+    if user is not None and generatorToken.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your Account has been Successfully Activated, You can connect now')
+        return redirect('login')
+    else:
+        messages.error(request, 'Error Occured, Try later!!!')
+        return redirect('home')
---
+++ authentification/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'goldendev24@gmail.com'
+EMAIL_HOST_PASSWORD = 'lyzy fnah qlma ekfx'
+EMAIL_PORT = 587
